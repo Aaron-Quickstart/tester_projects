bc1c0230be799c8fb1712e197fe4373eaeff2647
Aaron-Quickstart <aaron12clure@gmail.com>
2025-03-03 23:05:38 -0500

    fought with .js for a while.  currently will paint a square, but event listen is not working.

--- Accuracy_Clicker_Game/js/index.js
+++ Accuracy_Clicker_Game/js/index.js
@@ -1,11 +1,60 @@
-function gameFunction()
+function makeSquare()
 {
     // generate cordinates and pick a color
     let x = Math.floor(Math.random()*6);
     let y = Math.floor(Math.random()*6);
-    let z = Math.floor(Math.random()*4);
-    let colors = ['Red', 'Blue', 'Green', 'Yellow'];
+    let z = Math.floor(Math.random()*6);
+    let colors = ['Red', 'Red', 'Red', 'Red', 'Yellow', 'Green', 'Blue'];
+    let color1 = colors[z];
+
+    //console.log(x);
+    //console.log(y);
+   // console.log(color1);
+
+    // color selected cell and listen for click
+    let coords = document.querySelectorAll('#game_table td')
+    let paintedCoord = x *5 + y;
+    console.log(coords);
+    console.log(paintedCoord);
+    if (z <= 3)
+    {
+        coords[paintedCoord].classList.add('target');
+     }
+    else if (z === 4)
+    {
+        coords[paintedCoord].classList.add('yellowSquare');
+    }
+    else if (z === 5)
+    {
+        coords[paintedCoord].classList.add('greenSquare');
+    }
+    else if (z === 6)
+    {
+        coords[paintedCoord].classList.add('blueSquare');
+    }
+
     
-    let coords = document.querySelectorAll('game_table td')
-    cells.
+    return { x , y}
+}
+    
+function clickListen()
+{
+    let table = document.getElementById('game_table');
+    let currentTarget = null;
+    let score = document.getElementById('score_box');
+    let points = 0;
+    table.addEventListener('click', function(event) 
+    {
+        let clickedCell = event.target;
+        if (clickedCell.classList.contains('target')) 
+        {
+            points = points + 125;
+            score.innerText = points;
+            currentTarget = makeSquare();
+        }
+    })
+}
+function startGame()
+{
+    makeSquare();
 }
