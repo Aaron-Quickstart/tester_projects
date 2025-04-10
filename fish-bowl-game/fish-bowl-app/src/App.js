3af2f279474fd968a4cfdc0ee3049a3e97e95eec
aaron-quickstart <aaron12clure@gmail.com>
2025-04-09 22:22:33 -0400

    Routing has been completed

--- fish-bowl-game/fish-bowl-app/src/App.js
+++ fish-bowl-game/fish-bowl-app/src/App.js
@@ -3,14 +3,24 @@ import GameStartScreen from "./pages/GameStartScreen";
 import TeamsScreen from "./pages/TeamsScreen";
 import HowToPlay from "./pages/HowToPlay";
 import RoundStartScreen from "./pages/RoundStartScreen";
+import PlayerTurn from "./pages/PlayerTurn";
+import EndOfTurn from "./pages/EndOfTurn";
+import EndOfRound from "./pages/EndOfRound";
+import FinalScore from "./pages/FinalScore";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={< GameStartScreen />}/>
-        <Route path="/TeamsScreen" element={< TeamsScreen />}/>
-        <Route path="/HowToPlay" element={<HowToPlay />}/>
+        <Route path="/" element={< GameStartScreen/>}/>
+        <Route path="/TeamsScreen" element={< TeamsScreen/>}/>
+        <Route path="/HowToPlay" element={<HowToPlay/>}/>
+        <Route path="/RoundStartScreen" element={<RoundStartScreen/>}/>
+        <Route path="/PlayerTurn" element={<PlayerTurn/>}/>
+        <Route path="/RoundStartScreen" element={<RoundStartScreen/>}/>
+        <Route path="/EndOfTurn" element={<EndOfTurn/>}/>
+        <Route path="/EndOfRound" element={<EndOfRound/>}/>
+        <Route path="/FinalScore" element={<FinalScore/>}/>
       </Routes>
     </HashRouter>
 
