fe332858733b5d5e794de95ef961835b6b004bac
aaron-quickstart <aaron12clure@gmail.com>
2025-04-13 23:07:45 -0400

    added to rule book

--- fish-bowl-game/fish-bowl-app/src/pages/HowToPlay.js
+++ fish-bowl-game/fish-bowl-app/src/pages/HowToPlay.js
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import RuleBook from "./componets/RuleBook";
 
 
 
@@ -16,15 +15,17 @@ export default function howToPlay()
             <Link to="/"><button>🔙</button></Link>
             <div className="ruleBook">
                 <h2>How To Play:</h2>
-                <ul>
-                    <li>Playes will separate into teams.  Larger teams are more fun</li>
-                    <li>Like Charades, teams will take turns guessing what their actor is trying to communicate.</li>
-                    <li>Before each turn, the actor will have a chance to read the communication rules before starting the 60 second timer.</li>
-                    <li>The word will appear on screen once the timer has been started.  If the actor is unable to successfully communicate the word / phrase, there is a pass button</li>
-                    <li>The pass button will skip the term temporarily, effectively putting it on the bottom of the deck.</li>
-                    <li>Once the 60 seconds has expired, pass the device to the other team. This repeats until all players have acted with that rule set.</li>
-                </ul>
-                <RuleBook />
+                <div className="gameRules">
+                    <h3>Objective:</h3>
+                    <p>
+                        To win, obviously. Or at least to look less foolish than your friends 
+                        while shouting random nonsense and flailing like a caffeinated mime. 
+                        The game combines wordplay, acting, and desperation into a glorious mess 
+                        where you guess what in the world your friend is trying to communicate.
+                    </p>
+                    <h3>Getting Ready for the Chaos</h3>
+                    <p></p>
+                </div>
             </div>
             <Link to="/RoundStartScreen"><button>Start First Round</button></Link>
         </>
