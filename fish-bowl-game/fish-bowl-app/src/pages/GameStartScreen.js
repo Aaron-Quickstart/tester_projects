34d496dd0eb4b4d8c631cb3e70adf3a5d0104042
aaron-quickstart <aaron12clure@gmail.com>
2025-04-15 17:41:29 -0400

    centered content globally.  Started styles for GameStartScreen file

--- fish-bowl-game/fish-bowl-app/src/pages/GameStartScreen.js
+++ fish-bowl-game/fish-bowl-app/src/pages/GameStartScreen.js
@@ -10,11 +10,14 @@ export default function StartScreen()
 
     return(
         <>
+            <div className="startTitles">
             <h1 className="gameTitle">Hook Line and Thinker</h1>
             <h3 className="gameSubtitle">A mobile way to play Fish Bowl</h3>
-            <h4 classNAme="titlePageText">Read the rules or jump right into the fun.</h4>
-            <Link to="/teamsScreen"><button>Start Playing</button></Link>
-            <Link to="/HowToPlay"><button>Learn the Game</button></Link>
+            </div>
+            <div className="startScreenButtons">
+                <Link to="/teamsScreen" className="steamsScreenBtn"><button>Start Playing</button></Link>
+                <Link to="/HowToPlay" className="howToPlayBtn"><button>Learn the Game</button></Link>
+            </div>
         </>
     );
 }
