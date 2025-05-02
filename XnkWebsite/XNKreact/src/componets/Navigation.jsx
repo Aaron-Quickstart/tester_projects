1c6e3b44409586867e92279199740f0cb86a2d46
aaron-quickstart <aaron12clure@gmail.com>
2025-05-02 19:04:05 -0400

    created navigation and social media links

---
+++ XnkWebsite/XNKreact/src/componets/Navigation.jsx
@@ -0,0 +1,22 @@
+
+
+export default function Navigation()
+{
+
+
+    return(
+        <>
+            <div class='topRowNav'>
+                <a href="index.html">Home</a>
+                <a href="showSchedule.html">Show Schedule</a>
+                <a href="listen.html">Listen</a>
+            </div>
+            <br/>
+            <div class='bottomRowNav'>
+                <a href="watch.html">Watch</a>
+                <a href="contact.html">Contact Us</a>
+                <a href="bio.html">Bio</a>
+            </div>
+        </>
+    )
+}
