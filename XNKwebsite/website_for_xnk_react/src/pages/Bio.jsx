71e16dd91fea8bdc1ce44370e9b976907a06d3cf
aaron-quickstart <aaron12clure@gmail.com>
2025-05-04 15:27:30 -0400

    built bios page.

--- XNKwebsite/website_for_xnk_react/src/pages/Bio.jsx
+++ XNKwebsite/website_for_xnk_react/src/pages/Bio.jsx
@@ -1,5 +1,6 @@
 import NavigationLinks from "../componets/NavigationLinks";
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 
@@ -12,6 +13,76 @@ export default function Bio()
     <>
         <NavigationLinks/>
         <h1>This is the Bio Page</h1>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+                <Card.Title>Ryan</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Lead Vox | Rhythm Guitar</Card.Subtitle>
+                    <Card.Text>
+                        Ryan is a songwriter, entertainer, and artist newly based in Los Angeles. Originally from East TN.
+                        He is a groove and lyric based writer with a volcanic passion for dramatic rock n' roll. 
+                        However, he pulls influences from all over including blues, jazz, pop, gospel, and latin music.
+                        <br/><br/>
+                        "...simply put; I'm here to make people feel good. I love music, playing, singing,and writing. 
+                        I want to share it and take care of people. and if I positively influence someone then I'm succeeding."
+                    </Card.Text>
+            </Card.Body>
+        </Card>
+
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+                <Card.Title>Petre</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Lead Guitar</Card.Subtitle>
+                    <Card.Text>
+                        Petre is a multi-instrumentalist from Bucharest, Romania.<br/> 
+                        But don't let that eastern european sense of humor scare you. 
+                        Petre specializes in film scoring, production, the best schnitzel you've ever eaten, and face-melting guitar.
+                    </Card.Text>
+            </Card.Body>
+        </Card>
+
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+                <Card.Title>Riana</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Keytar | Synth</Card.Subtitle>
+                    <Card.Text>
+                        Riana is a Los Angeles native who's classical piano prowess has few rivals. 
+                        That combined with the attitude and delivery of a rock n' roller it's just as much fun to watch as to listen.
+                    </Card.Text>
+            </Card.Body>
+        </Card>
+
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+                <Card.Title>John</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Drums</Card.Subtitle>
+                    <Card.Text>
+                        John is a wicked good drummer from Massachusetts. 
+                        Boasting incredible chops in all styles from gospel, and jazz to metal, and latin.  He lives drums... and coffee.
+                    </Card.Text>
+            </Card.Body>
+        </Card>
+
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+                <Card.Title>Lina Cooper</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Bass | Backing Vox</Card.Subtitle>
+                    <Card.Text>
+                        Miss Lina Cooper is a singer/songwriter/popstar originally from Ukraine. 
+                        We kidnapped her and turns out she is a phenomenal bass player.  
+                        Her arranging and writing skills are unmatched. 
+                        Do not cross her; she will write a song about you.
+                        <br/><br/>
+                        "Music and songwriting have been my favorite things to do ever since I was about 8 years old. 
+                        I still can't believe I get to do it as a job. 
+                        And also dress up like I'm a rock star and make people gasp. dream job"
+                    </Card.Text>
+            </Card.Body>
+        </Card>
     </>
     )
 }
