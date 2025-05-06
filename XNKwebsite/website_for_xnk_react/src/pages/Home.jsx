7080ff16833c51385a2a00bfa2c8955dde8e7c0e
aaron-quickstart <aaron12clure@gmail.com>
2025-05-05 22:45:25 -0400

    added an icon to the browser tab.

--- XNKwebsite/website_for_xnk_react/src/pages/Home.jsx
+++ XNKwebsite/website_for_xnk_react/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import NavigationLinks from "../componets/NavigationLinks";
 import SocialLinks from "../componets/SocialLinks";
-
+import Background from "./../assets/XnkBackground.png";
 
 
 export default function Home()
@@ -12,6 +12,7 @@ export default function Home()
     <>
         <NavigationLinks/>
         <h1>This is the Home Page</h1>
+        <img src={Background} alt="Background"/>
         <SocialLinks/>
     </>
     )
