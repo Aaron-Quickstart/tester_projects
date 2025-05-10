8390dd923bd5d772c2216deccc3f115b878ca17e
aaron-quickstart <aaron12clure@gmail.com>
2025-05-10 18:41:47 -0400

    Reworked Bio Cards.  Timing intervals, background and text color, and sizing.

--- XNKwebsite/website_for_xnk_react/src/pages/Bio.jsx
+++ XNKwebsite/website_for_xnk_react/src/pages/Bio.jsx
@@ -22,63 +22,63 @@ export default function Bio()
         <br/>
         <h1>Meet the Band</h1>
         <Container className='flex-column d-flex nav align-items-center'>
-            <Carousel fade>
-                <Carousel.Item>
-                    <Card className='bg-transparent border-white' text='white' style={{ width: '35rem' }}>
+            <Carousel>
+                <Carousel.Item interval={10000}>
+                    <Card className='bg-transparent border-white' text='white' style={{ height: '60rem', width: '35rem' }}>
                         <Card.Img variant="top" src={Ryan} />
                         <Card.Body>
                             <Card.Title><h2>Ryan</h2></Card.Title>
                             <Card.Subtitle className="mb-2" style={{color: "grey"}}><h4>Lead Vox | Rhythm Guitar</h4></Card.Subtitle>
-                                <Card.Text>
-                                    Ryan is a songwriter, entertainer, and artist newly based in Los Angeles. Originally from East TN.
-                                    He is a groove and lyric based writer with a volcanic passion for dramatic rock n' roll. 
-                                    However, he pulls influences from all over including blues, jazz, pop, gospel, and latin music.
-                                <br/><br/>
-                                    "...simply put; I'm here to make people feel good. I love music, playing, singing,and writing. 
-                                    I want to share it and take care of people. and if I positively influence someone then I'm succeeding."
-                                </Card.Text>
+                            <Card.Text>
+                                Ryan is a songwriter, entertainer, and artist newly based in Los Angeles. Originally from East TN.
+                                He is a groove and lyric based writer with a volcanic passion for dramatic rock n' roll. 
+                                However, he pulls influences from all over including blues, jazz, pop, gospel, and latin music.
+                            <br/><br/>
+                                "...simply put; I'm here to make people feel good. I love music, playing, singing,and writing. 
+                                I want to share it and take care of people. and if I positively influence someone then I'm succeeding."
+                            </Card.Text>
                         </Card.Body>
                         <br/>
                     </Card>
                 </Carousel.Item>
 
-                <Carousel.Item>
-                    <Card className='bg-transparent border-white' text='white' style={{ width: '35rem' }}>
+                <Carousel.Item interval={10000}>
+                        <Card className='bg-transparent border-white' text='white' style={{ height: '60rem', width: '35rem' }}>
                         <Card.Img variant="top" src={Petre} />
                         <Card.Body>
                             <Card.Title><h2>Petre</h2></Card.Title>
                             <Card.Subtitle className="mb-2" style={{color: "grey"}}><h4>Lead Guitar</h4></Card.Subtitle>
-                                <Card.Text>
-                                        Petre is a multi-instrumentalist from Bucharest, Romania.<br/> 
-                                        But don't let that eastern european sense of humor scare you. 
-                                        Petre specializes in film scoring, production, the best schnitzel you've ever eaten, and face-melting guitar.
-                                </Card.Text>
+                            <Card.Text>
+                                Petre is a multi-instrumentalist from Bucharest, Romania.<br/> 
+                                But don't let that eastern european sense of humor scare you. 
+                                Petre specializes in film scoring, production, the best schnitzel you've ever eaten, and face-melting guitar.
+                            </Card.Text>
                         </Card.Body>
                         <br/>
                     </Card>
                 </Carousel.Item>
 
-                <Carousel.Item>
-                        <Card className='bg-transparent border-white' text='white' style={{ width: '35rem' }}>
+                <Carousel.Item interval={10000}>
+                        <Card className='bg-transparent border-white' text='white' style={{ height: '60rem', width: '35rem' }}>
                         <Card.Img variant="top" src={Riana} />
                         <Card.Body>
-                            <Card.Title>Riana</Card.Title>
-                                <Card.Subtitle className="mb-2" style={{ color: "grey" }}>Keytar | Synth</Card.Subtitle>
-                                <Card.Text>
-                                    Riana is a Los Angeles native who's classical piano prowess has few rivals. 
-                                    That combined with the attitude and delivery of a rock n' roller it's just as much fun to watch as to listen.
-                                </Card.Text>
+                        <Card.Title>Riana</Card.Title>
+                            <Card.Subtitle className="mb-2" style={{ color: "grey" }}>Keytar | Synth</Card.Subtitle>
+                            <Card.Text>
+                                Riana is a Los Angeles native who's classical piano prowess has few rivals. 
+                                That combined with the attitude and delivery of a rock n' roller it's just as much fun to watch as to listen.
+                            </Card.Text>
                         </Card.Body>
                         <br/>
                     </Card>
                 </Carousel.Item>
 
-                <Carousel.Item>
-                    <Card style={{ width: '30rem' }}>
+                <Carousel.Item interval={10000}>
+                        <Card className='bg-transparent border-white' text='white' style={{ height: '60rem', width: '30rem' }}>
                         <Card.Img variant="top" src={John} />
                         <Card.Body>
                             <Card.Title>John</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">Drums</Card.Subtitle>
+                            <Card.Subtitle className="mb-2" style={{ color: "grey" }}>Drums</Card.Subtitle>
                             <Card.Text>
                                 John is a wicked good drummer from Massachusetts.
                                 Boasting incredible chops in all styles from gospel, and jazz to metal, and latin.  He lives drums... and coffee.
@@ -87,12 +87,29 @@ export default function Bio()
                     </Card>
                 </Carousel.Item>
 
+                <Carousel.Item interval={10000}>
+                        <Card className='bg-transparent border-white' text='white' style={{ height: '60rem', width: '30rem' }}>
+                        <Card.Img variant="top" src={Lina} />
+                        <Card.Body>
+                            <Card.Title>Lina Cooper</Card.Title>
+                            <Card.Subtitle className="mb-2" style={{ color: "grey" }}>Bass | Backing Vox</Card.Subtitle>
+                            <Card.Text>
+                                Miss Lina Cooper is a singer/songwriter/popstar originally from Ukraine.
+                                We kidnapped her and turns out she is a phenomenal bass player.
+                                Her arranging and writing skills are unmatched.
+                                Do not cross her; she will write a song about you.
+                                <br /><br />
+                                "Music and songwriting have been my favorite things to do ever since I was about 8 years old.
+                                I still can't believe I get to do it as a job.
+                                And also dress up like I'm a rock star and make people gasp. dream job"
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Carousel.Item>
+
             </Carousel>
-
         </Container>
-
         <br/>
-
         <SignUpForm/>
         <FooterNote/>
     </>
@@ -103,23 +120,5 @@ export default function Bio()
 
 
 
-<Carousel.Item>
-    <Card style={{ width: '30rem' }}>
-        <Card.Img variant="top" src={Lina} />
-        <Card.Body>
-            <Card.Title>Lina Cooper</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Bass | Backing Vox</Card.Subtitle>
-                <Card.Text>
-                    Miss Lina Cooper is a singer/songwriter/popstar originally from Ukraine. 
-                    We kidnapped her and turns out she is a phenomenal bass player.  
-                    Her arranging and writing skills are unmatched. 
-                    Do not cross her; she will write a song about you.
-                    <br/><br/>
-                    "Music and songwriting have been my favorite things to do ever since I was about 8 years old. 
-                    I still can't believe I get to do it as a job. 
-                    And also dress up like I'm a rock star and make people gasp. dream job"
-                </Card.Text>
-        </Card.Body>
-    </Card>
-</Carousel.Item>
+
 */
