16f6423901298a645e6a01c0a145654b7c8d83bb
aaron-quickstart <aaron12clure@gmail.com>
2025-05-10 21:15:46 -0400

    started working on music page.

--- XNKwebsite/website_for_xnk_react/src/componets/HotelCafeMusic.jsx
+++ XNKwebsite/website_for_xnk_react/src/componets/HotelCafeMusic.jsx
@@ -1,6 +1,8 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import cover_art_liveHotelCafe from './../assets/cover_art_liveHotelCafe.jpg';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
 
 export default function HotelCafeMusic()
 {
@@ -9,15 +11,29 @@ export default function HotelCafeMusic()
 
 
   return (
-    <Card>
-      <Card.Img src={cover_art_liveHotelCafe} />
-      <Card.Body>
-        <Card.Title>Live at Hotel Cafe is out now!</Card.Title>
-        <Card.Text>
-          We are bringing back live albums.  Pretty groovy, eh?
-        </Card.Text>
-        <Button variant="primary">Listen Here</Button>
-      </Card.Body>
-    </Card>
+    <div>
+        <Card>
+            <Row>
+                <Col>
+                    <Card.Img src={cover_art_liveHotelCafe} style={{height: '200px', width: '200px'}} />
+                </Col>
+                <Col>
+                <Card.Body>
+                    <Card.Title>Live at Hotel Cafe is out now!</Card.Title>
+                    <Card.Text>
+                    We are bringing back live albums.  Pretty groovy, eh?
+                    </Card.Text>
+                    <Button variant="primary">Listen Here</Button>
+                </Card.Body>                
+                </Col>
+            </Row>
+        </Card>
+    </div>
+
   );
 }
+
+
+/*
+
+*/
