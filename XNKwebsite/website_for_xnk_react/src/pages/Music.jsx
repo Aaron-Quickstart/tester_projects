f41beadf0a1a10875f83e95f7971f6bd8efe5a93
aaron-quickstart <aaron12clure@gmail.com>
2025-05-11 15:39:22 -0400

    music page work.

--- XNKwebsite/website_for_xnk_react/src/pages/Music.jsx
+++ XNKwebsite/website_for_xnk_react/src/pages/Music.jsx
@@ -1,12 +1,10 @@
-import Container from "react-bootstrap/esm/Container";
 import FooterNote from "../componets/FooterNote";
 import NavigationLinks from "../componets/NavigationLinks";
 import SignUpForm from "../componets/SignupForm";
-import Row from "react-bootstrap/esm/Row";
-import Col from "react-bootstrap/esm/Col";
 import HotelCafeMusic from "../componets/HotelCafeMusic";
 import LoudMusic from "../componets/LoudMusic";
 import VampiresMusic from "../componets/VampireMusic";
+import RaincloudMusic from "../componets/RainCloudMusic";
 
 
 
@@ -23,22 +21,10 @@ export default function Music()
         <HotelCafeMusic/>
         <LoudMusic/>
         <VampiresMusic/>
+        <RaincloudMusic/>
         <br/>
         <SignUpForm/>
         <FooterNote/>
     </>
     )
 }
-
-/*
-        <Container>
-            <Row>
-                <Col>
-
-                </Col>
-                <Col>
-                
-                </Col>
-            </Row>
-        </Container>
-*/
