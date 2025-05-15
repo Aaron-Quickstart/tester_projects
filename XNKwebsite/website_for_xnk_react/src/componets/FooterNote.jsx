5415254ad7371ec531dc9bc3a342a85b6ef17f43
aaron-quickstart <aaron12clure@gmail.com>
2025-05-14 21:33:05 -0400

    contact page is nearing completion

--- XNKwebsite/website_for_xnk_react/src/componets/FooterNote.jsx
+++ XNKwebsite/website_for_xnk_react/src/componets/FooterNote.jsx
@@ -9,6 +9,7 @@ export default function FooterNote()
 
     return(
         <>
+            <br/><br/>
             <p>&copy; 2025 by XNK</p>
         </>
     )
--- XNKwebsite/website_for_xnk_react/src/pages/Contact.jsx
+++ XNKwebsite/website_for_xnk_react/src/pages/Contact.jsx
@@ -2,8 +2,10 @@ import Form from 'react-bootstrap/Form';
 import Container from "react-bootstrap/esm/Container";
 import Button from 'react-bootstrap/Button';
 
+import FullBand from './../assets/photo_fullBAnd.jpg';
 import FooterNote from "../componets/FooterNote";
 import NavigationLinks from "../componets/NavigationLinks";
+import ContactForm from '../componets/ContactForm';
 
 
 
@@ -17,8 +19,20 @@ export default function Contact()
     <>
     <NavigationLinks/>
         <h1>Want us to come rock for you?</h1>
-        <Container>
-            <Form action=''>
+        <img src={FullBand} className='mb-5' style={{borderRadius: '1.5rem'}}/>
+
+
+        <ContactForm/>
+        <FooterNote/>
+    </>
+        
+    
+    )
+}
+
+/*
+
+            <Form>
                 <Form.Group className="mb-3" controlId="userEmail">
                 <Form.Label>Your information will not be shared with anyone.</Form.Label>
                 <Form.Control type="email" placeholder="your email here" />
@@ -28,31 +42,9 @@ export default function Contact()
                 <Form.Control as="textarea" rows={3} />
                 </Form.Group>
                 <Button>Send Message</Button>
-            </Form>  
-        </Container>
+            </Form>
 
-        <br/><br/>
-        <FooterNote/>
-    </>
-        
-    
-    )
-}
 
-/*
-    <form id="request-form" action="https://formspree.io/f/mbldzzbg" method="POST">
-        <label for="fname">*&nbsp;First name:</label><br/>
-        <input type="text" id="fname" name="fname" required/><br/>
 
-        <label for="lname">Last name:</label><br/>
-        <input type="text" id="lname" name="lname"/><br/>
 
-        <label for="customer-email">*&nbsp;Email:</label><br/>
-        <input type="text" id="customer-email" name="customer-email" required/><br/>
-
-        <label for="recipe-request-field">*&nbsp;Request a recipe here.</label><br/>
-        <input id="recipe-request-field" name="recipe-request-field"  size="50" required/><br/>
-
-        <input type="submit" value="Submit">
-    <form/>
 */
